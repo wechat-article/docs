d10ae55a09721d6158507dc3882a9e80456b037f
jock <findsource@proton.me>
2025-02-22 21:11:08 +0800

    update

--- server.ts
+++ server.ts
@@ -3,8 +3,14 @@
 import { serveDir, serveFile } from "@std/http/file-server";
 
 Deno.serve((req: Request) => {
+    const pathname = new URL(req.url).pathname;
+
+    let headers = []
+    if (pathname.startsWith('/assets/')) {
+        headers = ['Cache-Control: max-age=31536000,immutable']
+    }
     return serveDir(req, {
         fsRoot: ".vitepress/dist",
-        // headers: ['Cache-Control: max-age=31536000,immutable']
+        headers: headers,
     });
 });

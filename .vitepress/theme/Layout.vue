cab5b4b62113472627f6454f0f882058b1ad81ab
jock <findsource@proton.me>
2025-08-30 22:06:03 +0800

    update

--- .vitepress/theme/Layout.vue
+++ .vitepress/theme/Layout.vue
@@ -1,12 +1,47 @@
-<script setup>
+<script lang="ts" setup>
 import DefaultTheme from "vitepress/theme";
-import { onMounted } from "vue";
-import { useRouter } from "vitepress";
+import {nextTick, onMounted, provide} from "vue";
+import {useData, useRouter} from "vitepress";
 import mediumZoom from "medium-zoom";
 import Comments from "./Comments.vue";
 
-const { Layout } = DefaultTheme;
+const {Layout} = DefaultTheme;
 const router = useRouter();
+
+const {isDark} = useData()
+
+const enableTransitions = () => {
+  if ('startViewTransition' in document) {
+    return window.matchMedia('(prefers-reduced-motion: no-preference)').matches
+  }
+}
+
+provide('toggle-appearance', async ({clientX: x, clientY: y}: MouseEvent) => {
+  if (!enableTransitions()) {
+    isDark.value = !isDark.value;
+    return
+  }
+
+  const clipPath = [
+      `circle(0px at ${x}px ${y}px)`,
+      `circle(${Math.hypot(Math.max(x, innerWidth - x), Math.max(y, innerHeight - y))}px at ${x}px ${y}px)`,
+  ]
+
+  await document.startViewTransition(async () => {
+    isDark.value = !isDark.value;
+    await nextTick()
+  }).ready
+
+  document.documentElement.animate(
+      {clipPath: isDark.value ? clipPath.reverse() : clipPath},
+      {
+        duration: 500,
+        easing: 'ease-in-out',
+        fill: 'forwards',
+        pseudoElement: `::view-transition-${isDark.value ? 'old' : 'new'}(root)`
+      }
+  )
+})
 
 // Setup medium zoom with the desired options
 const setupMediumZoom = () => {
@@ -25,7 +60,7 @@ router.onAfterRouteChanged = setupMediumZoom;
 <template>
   <Layout>
     <template #doc-after>
-      <Comments />
+      <Comments/>
     </template>
   </Layout>
 </template>
@@ -38,5 +73,29 @@ router.onAfterRouteChanged = setupMediumZoom;
 .medium-zoom-overlay,
 .medium-zoom-image--opened {
   z-index: 999;
+}
+
+::view-transition-old(root),
+::view-transition-new(root) {
+  animation: none;
+  mix-blend-mode: normal;
+}
+
+::view-transition-old(root),
+.dark::view-transition-new(root) {
+  z-index: 1;
+}
+
+::view-transition-new(root),
+.dark::view-transition-old(root) {
+  z-index: 9999;
+}
+
+.VPSwitchAppearance {
+  width: 22px !important;
+}
+
+.VPSwitchAppearance .check {
+  transform: none !important;
 }
 </style>
